d0c327e59c8751774912a0b1c5b95932675f7a54
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-04-05 20:09:23 +0300

    add ios test

--- browserstack_test/pages/main_page.py
+++ browserstack_test/pages/main_page.py
@@ -6,16 +6,24 @@ from appium.webdriver.common.appiumby import AppiumBy
 class MainPage:
 
     def search_request(self, query):
-        with step(f'Search request by word {query}'):
+        with step(f'ANDROID: Search request by word {query}'):
             browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
             browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(query)
 
+    def search_request_ios(self, query):
+        with step(f'iOS: Searsh request by word {query}'):
+            browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Button")).click()
+            browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Input")).send_keys(query + '\n')
+
+    def checking_result_ios(self, query):
+            browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Output")).should(have.text(query))
+
     def select_result_query(self):
-        with step('Select request result'):
+        with step('ANDROID: Select request result'):
             browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).click()
 
     def checking_result(self, query):
-        with step(f'Checking request result by word {query}'):
+        with step(f'ANDROID: Checking request result by word {query}'):
             results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
             results.should(have.size_greater_than(0))
             results.first.should(have.text(query))
--- tests/ios_app/test_search_ios.py
+++ tests/ios_app/test_search_ios.py
@@ -1,7 +1,13 @@
 from selene import browser, have
 from appium.webdriver.common.appiumby import AppiumBy
+from browserstack_test.pages.main_page import main_page
+
+
+# def test_search_ios_platform():
+#     browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Button")).click()
+#     browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Input")).send_keys("QaGuru" + '\n')
+#     browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Output")).should(have.text("QaGuru"))
 
 def test_search_ios_platform():
-    browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Button")).click()
-    browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Input")).send_keys("QaGuru" + '\n')
-    browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Output")).should(have.text("QaGuru"))
+    main_page.search_request_ios("QaGuru")
+    main_page.checking_result_ios("QaGuru")
