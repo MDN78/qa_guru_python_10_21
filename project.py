e9be36fd215857ee4cfce0586105c0153e25a0dc
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-04-08 22:12:03 +0300

    rebuild test for local mobile app

--- project.py
+++ project.py
@@ -9,8 +9,23 @@ load_dotenv()
 class Config(BaseSettings):
     USER_NAME: str = os.getenv('USER_NAME')
     ACCESS_KEY: str = os.getenv('ACCESS_KEY')
-    URL: str = os.getenv('URL')
     TIMEOUT: float = os.getenv('TIMEOUT')
-
+    # CONTEXT: list = os.getenv('CONTEXT')
+    # # app: str = path.relative_from_root('apk_file/org-wikipedia.apk')
+    platformName: str = os.getenv('PLATFORM_NAME')
+    appWaitActivity: str = os.getenv('APP_WAIT_ACTIVITY')
+    URL: str = os.getenv('URL')
+    # app: str = "D:\\QAGuru\\org-wikipedia.apk"
+    app: str = path.relative_from_root('apk_file/org-wikipedia.apk')
 
 config = Config(_env_file=path.relative_from_root(f'.env.{Config()}'))
+
+
+# class Config_local(BaseSettings):
+#     load_dotenv('.env.local_emulator')
+#     platformName: str = os.getenv('PLATFORM_NAME')
+#     appWaitActivity: str = os.getenv('APP_WAIT_ACTIVITY')
+#     app: str = path.relative_from_root('apk_file/org-wikipedia.apk')
+#     URL: str = os.getenv('URL')
+#
+# config_local = Config_local(_env_file=path.relative_from_root(f'.env.local_emulator.{Config_local()}'))
--- tests/android_app/conftest.py
+++ tests/android_app/conftest.py
@@ -1,32 +1,52 @@
 import pytest
 import project
+from utils import path
 from utils import attach
 from appium import webdriver
 from selene import browser
 from allure_commons._allure import step
 from appium.options.android import UiAutomator2Options
 
+DEFAULT_CONTEXT = 'local'
+
+def pytest_addoption(parser):
+    parser.addoption('--context', default='local')
+
+# @pytest.fixture
+# def context(request):
+#     return request.config.getoption("--context")
 
 @pytest.fixture(scope='function', autouse=True)
-def mobile_management():
+def mobile_management(request):
     with step('Configurate options'):
-        options = UiAutomator2Options().load_capabilities({
-            # Specify device and os_version for testing
-            "platformName": "Android",
-            "appWaitActivity": "org.wikipedia.*",
-            # Set URL of the application under test
-            "app": "D:\\QAGuru\\org-wikipedia.apk",
-
-        })
-
-    browser.config.driver = webdriver.Remote(project.config.URL, options=options)
-    browser.config.timeout = project.config.TIMEOUT
+        context = request.config.getoption('--context')
+        context = context if context != '' else DEFAULT_CONTEXT
+        if context == 'local':
+            options = UiAutomator2Options()
+            # options.set_capability('remote_url', project.config.URL)
+            options.set_capability("platformName", project.config.platformName)
+            options.set_capability("appWaitActivity", project.config.appWaitActivity)
+            options.set_capability("app", project.config.app)
+        browser.config.driver = webdriver.Remote(project.config.URL, options=options)
+        browser.config.timeout = project.config.TIMEOUT
+    #     options = UiAutomator2Options().load_capabilities({
+    #         # Specify device and os_version for testing
+    #         "platformName": "Android",
+    #         "appWaitActivity": "org.wikipedia.*",
+    #         # Set URL of the application under test
+    #         "app": "D:\\QAGuru\\org-wikipedia.apk",
+    #         # "app": path.relative_from_root('apk_file/org-wikipedia.apk'),
+    #
+    #     })
+    #
+    # browser.config.driver = webdriver.Remote(project.config.URL, options=options)
+    # browser.config.timeout = project.config.TIMEOUT
 
     yield
 
-    # with step('Add screenshot'):
-    #     attach.add_screenshot(browser)
-    #
+    with step('Add screenshot'):
+        attach.add_screenshot(browser)
+
     # with step('Add html'):
     #     attach.add_html(browser)
     #
