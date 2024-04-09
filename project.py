9239cbbb2f9ce9d2e4f28ea801331a92a3c1b2f0
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-04-09 20:41:44 +0300

    update tests

--- project.py
+++ project.py
@@ -10,10 +10,12 @@ class Config(BaseSettings):
     ACCESS_KEY: str = os.getenv('ACCESS_KEY')
     TIMEOUT: float = os.getenv('TIMEOUT')
     platformName: str = os.getenv('PLATFORM_NAME')
+    platform_version: str = os.getenv('PLATFORM_VERSION')
+    deviceName: str = os.getenv('DEVICE_NAME')
     appWaitActivity: str = os.getenv('APP_WAIT_ACTIVITY')
     URL: str = os.getenv('URL')
     app: str = path.relative_from_root('apk_file/org-wikipedia.apk')
-    load_dotenv('.env.example')
+    load_dotenv('.env.local_real')
 
     def to_driver_options(self, context):
         options = UiAutomator2Options()
@@ -22,6 +24,22 @@ class Config(BaseSettings):
             options.set_capability("platformName", self.platformName)
             options.set_capability("appWaitActivity", self.appWaitActivity)
             options.set_capability("app", self.app)
+        if context == 'bstack':
+            options.set_capability('remote_url', self.URL)
+            options.set_capability('deviceName', self.deviceName)
+            options.set_capability("platformName", self.platformName)
+            options.set_capability('platformVersion', self.platform_version)
+            options.set_capability("app", self.app)
+            options.set_capability(
+                'bstack:options',
+                {
+                    "projectName": "First Python project",
+                    "buildName": "browserstack-build-1",
+                    "sessionName": "BStack first_test",
+                    "userName": self.USER_NAME,
+                    "accessKey": self.ACCESS_KEY,
+                },
+            )
         return options
 
 
